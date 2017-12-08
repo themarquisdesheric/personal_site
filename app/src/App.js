68f5e7796c5f22573f689b4568e8dc91eaf55bb0
Yuval Allweil <themarquisdesheric@gmail.com>
2017-12-07 20:26:08 -0800

    smooth scroll time

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import FirstRow from './layout/FirstRow';
+import SecondRow from './layout/SecondRow';
 
 class App extends Component {
   render() {
     return (
       <div>
         <FirstRow />
+        <SecondRow />
       </div>
     );
   }
