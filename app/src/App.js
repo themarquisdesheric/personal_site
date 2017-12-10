af54b07f36035ca6d3004bc0fb10fa0377bdf240
Yuval Allweil <themarquisdesheric@gmail.com>
2017-12-09 20:18:28 -0800

    implemented smooth scroll to each row

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import './App.css'; 
 import FirstRow from './layout/FirstRow';
-import WhenInView from './components/WhenInView';
+import SecondRow from './layout/SecondRow';
+import ThirdRow from './layout/ThirdRow';
+import Waypoint from 'react-waypoint';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  scrollToNode = (previousPosition, node) => {
+    if (previousPosition === Waypoint.below) {
+      node.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
   render() {
     return (
       <div>
         <FirstRow />
-        <WhenInView />
+        <div ref={row => this.secondRow = row}>
+          <Waypoint onEnter={({ previousPosition }) => this.scrollToNode(previousPosition, this.secondRow)} />
+          <SecondRow />
+        </div>
+        <div ref={row => this.thirdRow = row}>
+          <Waypoint onEnter={({ previousPosition }) => this.scrollToNode(previousPosition, this.thirdRow)} />
+          <ThirdRow />
+        </div>
       </div>
     );
   }
 }
+
+Waypoint.propTypes = {
+  onEnter: PropTypes.func.isRequired
+};
 
 export default App;
--- app/src/layout/SecondRow.js
+++ app/src/layout/SecondRow.js
@@ -6,25 +6,31 @@ import Hero from '../components/Hero';
 import Image from '../components/Image';
 
 const SecondRow = () => (
-  <Block>
-    <Columns>
-      <Column>
-        <Hero classes="is-fullheight">
-          <div className="hero-body">
-            <div></div>
-            <h1 className="title is-1">
-              SECOND ROW
-            </h1>
-          </div>
-        </Hero>
-      </Column>
-      <Column classes="is-paddingless">
-        <Image>
-          <img src="http://www.not-studio.com/images/not-studio-home.jpg" alt="img" />
-        </Image>
-      </Column>
-    </Columns>
-  </Block>
+  <div>
+    <Block>
+      <Columns>
+        <Column>
+          <Hero classes="is-fullheight">
+            <div className="hero-body">
+              <h1 className="title is-1">
+                <div>
+                  <h2 className="subtitle has-text-centered">
+                    subtitle
+                  </h2>
+                </div>
+                SECOND ROW
+              </h1>
+            </div>
+          </Hero>
+        </Column>
+        <Column classes="is-paddingless">
+          <Image>
+            <img src="https://mail.google.com/mail/u/0/?ui=2&ik=a76ce05d90&view=fimg&th=16037b0c49e02aae&attid=0.1.1&disp=emb&attbid=ANGjdJ9R2GTAi4ZuI_nD5nwqBdsV4__Kxy_wvHQwfz7FlMmQyB35LUwwSpxvBrhUGT1eTPbEEIJlHEKa2l_cON1WeFFwOvQLW7Oz0oM1DkRaZ_HpJoscPzx6aTYSY1w&sz=w1660-h2076&ats=1512878331006&rm=16037b0c49e02aae&zw&atsh=1" alt="yuval-orange-hat" />
+          </Image>
+        </Column>
+      </Columns>
+    </Block>
+  </div>
 );
 
 export default SecondRow;
