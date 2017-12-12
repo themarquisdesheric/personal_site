5c812886b9dede2849e18e2170715cf8de48ab98
Yuval Allweil <themarquisdesheric@gmail.com>
2017-12-12 11:32:26 -0800

    fixed proptype warning

--- app/src/components/Column.jsx
+++ app/src/components/Column.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Column = ({ children, classes = '' }) => (
+const Column = ({ children, classes }) => (
   <div className={`column ${classes}`}>
     {children}
   </div>
 );
 
+Column.defaultProps = {
+  classes: ''
+};
+
 Column.propTypes = {
   children: PropTypes.node.isRequired,
-  classes: PropTypes.string.isRequired
+  classes: PropTypes.string
 };
 
 export default Column;
