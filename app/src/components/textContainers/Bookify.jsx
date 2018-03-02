59657b7cc658c723df19eb6353b060cf45c93107
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-02 15:51:49 -0800

    added Bookify to projects

---
+++ app/src/components/textContainers/Bookify.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Bookify = () => (
+  <div className="content has-text-centered">
+    <p>Bookify is a tool to manage the books in your life.</p>
+
+    <p>
+      It is written in JavaScript, HTML, and Sass. Its tech stack is comprised of React for the view, Redux for state management, the Google Books API for search, react-sortable-hoc for drag-n-drop, and the HTML5 Local Storage API for persistence. It uses Jest for testing and was developed in Node.
+    </p> 
+  </div>
+);
+
+export default Bookify;
