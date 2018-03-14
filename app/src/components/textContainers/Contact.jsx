e94617a0a8027efb759995600676a52b44919754
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-14 09:48:16 -0700

    added transition to contact

--- app/src/components/textContainers/Contact.jsx
+++ app/src/components/textContainers/Contact.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Waypoint from 'react-waypoint';
 import 'font-awesome/css/font-awesome.css';
 import Icon from '../Icon';
 
-const Contact = () => {
-  const icons = [
-    {
-      href: 'mailto:themarquisdesheric@gmail.com', 
-      classes: 'fa fa-envelope-o',
-      text: 'themarquisdesheric@gmail.com'
-    },
-    {
-      href: '/YuvalAllweilResume.pdf',
-      target: '_blank', 
-      classes: 'fa fa-file-text-o',
-      text: 'Resume'
-    },
-    {
-      href: 'https://www.linkedin.com/in/yuval-allweil/',
-      classes: 'fa fa-linkedin-square linkedin',
-      text: 'LinkedIn'
-    },
-    {
-      href: 'https://github.com/themarquisdesheric', 
-      classes: 'fa fa-github-square',
-      text: 'GitHub'
-    }
-  ];
+class Contact extends Component {
+  render() {
+    const icons = [
+      {
+        href: 'mailto:themarquisdesheric@gmail.com', 
+        classes: 'fa fa-envelope-o',
+        text: 'themarquisdesheric@gmail.com'
+      },
+      {
+        href: '/YuvalAllweilResume.pdf',
+        target: '_blank', 
+        classes: 'fa fa-file-text-o',
+        text: 'Resume'
+      },
+      {
+        href: 'https://www.linkedin.com/in/yuval-allweil/',
+        classes: 'fa fa-linkedin-square linkedin',
+        text: 'LinkedIn'
+      },
+      {
+        href: 'https://github.com/themarquisdesheric', 
+        classes: 'fa fa-github-square',
+        text: 'GitHub'
+      }
+    ];
+    
+    return (
+      <div>
+        <div className="content has-text-centered">
+          <div className="icons">
 
-  return (
-    <div>
-      <div className="content has-text-centered">
-        <div className="icons">
-          {icons.map( (icon, i) => 
-            <Icon {...icon} key={icon.text} i={i} />
-          )}
+            <Waypoint 
+              onEnter={() => this.heading.classList.add('animate-contact')}
+              onLeave={() => this.heading.classList.remove('animate-contact')}
+            >
+              <p ref={heading => this.heading = heading} className="contact-heading">get in touch</p>
+            </Waypoint>
+            
+            {icons.map( (icon, i) => 
+              <Icon {...icon} key={icon.text} i={i} />
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Contact;
