746409b9a5cc65cea5be5d81fb7759a3aa319252
Yuval Allweil <themarquisdesheric@gmail.com>
2018-03-23 18:12:53 -0700

    calcRepoTotal and calcLangPercentages tests failing

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,50 @@ describe('github dashboard', () => {
     JavaScript: 500
   };
 
+  it('should calculate the sum of all languages for each repo', () => {
+    const expectedResult = 1000;
+
+    const repoTotal = calcRepoTotal(repo);
+
+    expect(repoTotal).toEqual(expectedResult);
+  });
+
+  it('should calculate the percentages of each language for each repo', () => {
+    const repoWithTotal = {
+      ...repo, 
+      total: 1000 
+    };
+
+    const expectedResult = {
+      HTML: 30,
+      CSS: 20,
+      JavaScript: 50
+    };
+
+    const result = calcLangPercentages(repoWithTotal);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should calculate the percentages of each language for each repo, and round the output', () => {
+    const repoWithTotal = {
+      HTML: 420, 
+      CSS: 250, 
+      JavaScript: 700,
+      total: 1370 
+    };
+
+    const expectedResult = {
+      HTML: 31,
+      CSS: 18,
+      JavaScript: 51
+    };
+
+    const result = calcLangPercentages(repoWithTotal);
+
+    expect(result).toEqual(expectedResult);
+  });
+
   it('should calculate the totals for each language', () => {
     const prevRepo = {
       HTML: 700, 
