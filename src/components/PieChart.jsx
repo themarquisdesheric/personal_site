ed98d0899fcc7782b3f63e6841818035f41c8bcf
Yuval Allweil <themarquisdesheric@gmail.com>
2021-05-23 15:26:20 -0700

    fix github logo on pie chart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -97,7 +97,6 @@ class PieChart extends Component {
         text: 'Data obtained via the GitHub API',
       },
       tooltips: false,
-      cutoutPercentage: this.calcCutOutSize(window.innerWidth)
     };
     
     const chart = new Chart(this.canvas, {
@@ -121,47 +120,15 @@ class PieChart extends Component {
     this.chart.destroy();
   }
 
-  setBackground = (screenX) => {
-    if (screenX > 850) {
-      return `url('github-large.png') no-repeat 50% ${this.calcBackgroundOffset(screenX)}`;
-    } else if (screenX > 720) {
-      return `url('github-small.png') no-repeat 50% ${this.calcBackgroundOffset(screenX)}`;
-    } else return 'transparent';
-  }
-
-  calcCutOutSize = (screenX) => {
-    if (screenX > 1340) return 24;
-    else if (screenX > 1270) return 26;
-    else if (screenX > 1200) return 28;
-    else if (screenX > 1150) return 29;
-    else if (screenX > 1080) return 31;
-    else if (screenX > 1010) return 34;
-    else if (screenX > 970) return 35;
-    else if (screenX > 900) return 39;
-    else if (screenX > 850) return 43;
-    else if (screenX > 800) return 24;
-    else if (screenX > 768) return 26;
-    else if (screenX > 720) return 28;
-    else return 0;
-  }
-
-  calcBackgroundOffset = (screenX) => {
-    if (screenX > 1080) return '49.9%';
-    else if (screenX > 850) return '49.9%';
-    else return '50.6%';
-  }
-
   render () {
     const { stats } = this.props;
 
     return (
       <div id="chart">
-        <canvas 
-          ref={canvas => this.canvas = canvas} 
-          style={{ 
-            background: this.setBackground(window.innerWidth) 
-          }}
-        />        
+        <div className="logo-wrapper">
+          <img src="/github-large.png" />
+          <canvas ref={canvas => this.canvas = canvas} />        
+        </div>
         
         <section>
           {stats.react
